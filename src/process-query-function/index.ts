a6e9b78cdc4145895ec07775b06dd4548c3a6f7a
Jaydeep Solanki <jaydp17@gmail.com>
2017-04-09 02:07:50 +0530

    search input text and return search result titles

--- src/process-query-function/index.ts
+++ src/process-query-function/index.ts
@@ -5,21 +5,31 @@
 import { inspect } from 'util';
 import { invokeMessengerReply, invokeProcessQuery } from '../common/lambda-utils';
 import * as MessengerAPI from '../common/messenger.api';
+import * as TraktAPI from './apis/trakt.api';
 
 // types
 import { LambdaCallback, LambdaEvent } from '../common/aws-lambda-types';
 import { IMessage, Platform } from '../common/internal-message-types';
 import { AnyMessagingObject, ITextMessageMessaging } from '../common/messenger-types';
 
-export function handler(event: IMessage, context: {}, callback: LambdaCallback): void {
+async function process(text: string): Promise<string> {
+  const searchResults = await TraktAPI.searchShow(text);
+  return searchResults
+    .map(result => result.show.title)
+    .join(', ');
+}
+
+export async function handler(event: IMessage, context: {}, callback: LambdaCallback): Promise<void> {
   console.log('entry', event); // tslint:disable-line:no-console
 
   const { text, platform } = event;
 
+  const result = await process(text);
+
   let promise: Promise<any>; // tslint:disable-line:no-any
   switch (platform) {
     case Platform.Messenger: {
-      promise = invokeMessengerReply(event);
+      promise = invokeMessengerReply({ ...event, text: result });
       break;
     }
     default:
