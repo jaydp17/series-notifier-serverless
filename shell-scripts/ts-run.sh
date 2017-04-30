ca45efa42c35c38c984ae7af4b94b54215288683
Jaydeep Solanki <jaydp17@gmail.com>
2017-04-30 20:06:55 +0530

    provide a SERVERLESS_LOCAL env to determine if code it running locally

--- shell-scripts/ts-run.sh
+++ shell-scripts/ts-run.sh
@@ -2,12 +2,18 @@
 
 . shell-scripts/parse_yaml.sh
 
-if [ ! -f secret-dev.yml ]; then
-  echo -e "${RED}Error: no secret-dev.yml file found!"
+fileName="secret-dev.yml"
+
+if [ "${NODE_ENV}" == "production" ]; then
+  fileName="secret-production.yml"
+fi
+
+if [ ! -f ${fileName} ]; then
+  echo -e "${RED}Error: no ${fileName} file found!"
   exit 0
 fi
 
 # read yaml file
-eval $(parse_yaml secret-dev.yml)
+eval $(parse_yaml ${fileName})
 
 ts-node --compilerOptions '{"module": "commonjs", "target":"es2015"}' "$@"
